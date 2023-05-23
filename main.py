0ade8588e1b4be772b7c7b3591770cf1fa63092f
Louise <idrisova_loui@mail.ru>
2023-05-23 11:36:10 +0300

    new

--- main.py
+++ main.py
@@ -3,187 +3,104 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import preprocessing
 
-df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
-df = df.drop(columns = ['anaemia','diabetes','high_blood_pressure','sex','smoking','time','DEATH_EVENT'])
+# read data and drop unnecessary columns
+data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+data = data.drop(columns = ['anaemia','diabetes','high_blood_pressure','sex','smoking','time','DEATH_EVENT'])
 
+# define number of bins
 n_bins = 20
 
-#данные до стандартизации
+# plot data before standardization
 fig1, axs = plt.subplots(2,3)
-axs[0, 0].hist(df['age'].values, bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(df['creatinine_phosphokinase'].values, bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(df['ejection_fraction'].values, bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(df['platelets'].values, bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(df['serum_creatinine'].values, bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(df['serum_sodium'].values, bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
-# plt.show()
+columns = ['age', 'creatinine_phosphokinase', 'ejection_fraction', 'platelets', 'serum_creatinine', 'serum_sodium']
+for i, column in enumerate(columns):
+    axs[i//3, i%3].hist(data[column].values, bins = n_bins)
+    axs[i//3, i%3].set_title(column)
 
-data = df.to_numpy(dtype='float') #стандартизация
+# standardize data
 scaler = preprocessing.StandardScaler().fit(data)
 data_scaled = scaler.transform(data)
 
-#данные после стандартизации
+# plot data after standardization
 fig2, axs = plt.subplots(2,3)
-axs[0, 0].hist(data_scaled[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(data_scaled[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(data_scaled[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(data_scaled[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(data_scaled[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(data_scaled[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
-# plt.show()
+for i, column in enumerate(columns):
+    axs[i//3, i%3].hist(data_scaled[:,i], bins = n_bins)
+    axs[i//3, i%3].set_title(column)
 
-print(df.mean()) #матожидание
-print(df.std()) #ско
+# calculate mean and standard deviation
+print(data.mean())
+print(data.std())
 
-#мо подсчитанное руками
-#массив значений математического ожидания до и после стандартизации
+# calculate mean and standard deviation by hand
 result_b_a=[[0,0,0,0,0,0],[0,0,0,0,0,0]]
-#нахождение МО
 for i in range(len(data)):
-    result_b_a[0][0]+=data[i][0]
-    result_b_a[0][1]=data[i][1]
-    result_b_a[0][2]=data[i][2]
-    result_b_a[0][3]=data[i][3]
-    result_b_a[0][4]=data[i][4]
-    result_b_a[0][5]=data[i][5]
-    result_b_a[1][0] += data_scaled[i][0]
-    result_b_a[1][1] = data_scaled[i][1]
-    result_b_a[1][2] = data_scaled[i][2]
-    result_b_a[1][3] = data_scaled[i][3]
-    result_b_a[1][4] = data_scaled[i][4]
-    result_b_a[1][5] = data_scaled[i][5]
+    for j in range(len(columns)):
+        result_b_a[0][j] += data.iloc[i][j]
+        result_b_a[1][j] += data_scaled[i][j]
 for i in range(len(result_b_a)):
     for j in range(len(result_b_a[i])):
-        result_b_a[i][j]/=len(data)
+        result_b_a[i][j] /= len(data)
     print(result_b_a[i])
 
-#приведение к диапазону с помощью minmaxscaler
+# transform data using MinMaxScaler
 min_max_scaler = preprocessing.MinMaxScaler().fit(data)
 data_min_max_scaled = min_max_scaler.transform(data)
 fig3, axs = plt.subplots(2,3)
-axs[0, 0].hist(data_min_max_scaled[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(data_min_max_scaled[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(data_min_max_scaled[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(data_min_max_scaled[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(data_min_max_scaled[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(data_min_max_scaled[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
-# plt.show()
+for i, column in enumerate(columns):
+    axs[i//3, i%3].hist(data_min_max_scaled[:,i], bins = n_bins)
+    axs[i//3, i%3].set_title(column)
 
-#приведение к диапазону с помощью maxabsscaler
+# transform data using MaxAbsScaler
 max_abs_scaler = preprocessing.MaxAbsScaler().fit(data)
 data_max_abs_scaler = max_abs_scaler.transform(data)
 fig4, axs = plt.subplots(2,3)
-axs[0, 0].hist(data_max_abs_scaler[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(data_max_abs_scaler[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(data_max_abs_scaler[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(data_max_abs_scaler[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(data_max_abs_scaler[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(data_max_abs_scaler[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
-# plt.show()
+for i, column in enumerate(columns):
+    axs[i//3, i%3].hist(data_max_abs_scaler[:,i], bins = n_bins)
+    axs[i//3, i%3].set_title(column)
 
-#приведение к диапазону с помощью robustscaler
-robust_scaler = preprocessing.RobustScaler().fit(data)
-data_robust_scaler = robust_scaler.transform(data)
+# robustscaler
+rs = preprocessing.RobustScaler().fit(data)
+data_rs = rs.transform(data)
 fig5, axs = plt.subplots(2,3)
-axs[0, 0].hist(data_robust_scaler[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(data_robust_scaler[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(data_robust_scaler[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(data_robust_scaler[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(data_robust_scaler[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(data_robust_scaler[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
+for i, ax in enumerate(axs.flatten()):
+    ax.hist(data_rs[:,i], bins = n_bins)
+    ax.set_title(data.columns[i])
 # plt.show()
 
-#стандартизация -5 10
-df_st=df[['age', 'creatinine_phosphokinase',
-          'ejection_fraction', 'platelets',
-          'serum_creatinine', 'serum_sodium']]
-df[['age', 'creatinine_phosphokinase', 'ejection_fraction', 'platelets', 'serum_creatinine', 'serum_sodium'] ]= (df_st-df_st.min())/(df_st.max()-df_st.min())*15-5
-print(df.max())
-print(df.min())
+#stand
+df_st = data[['age', 'creatinine_phosphokinase', 'ejection_fraction', 'platelets', 'serum_creatinine', 'serum_sodium']]
+df_norm = (df_st - df_st.min()) / (df_st.max() - df_st.min())
+df_norm = df_norm * 15 - 5
+print(df_norm.max())
+print(df_norm.min())
+data[['age', 'creatinine_phosphokinase', 'ejection_fraction', 'platelets', 'serum_creatinine', 'serum_sodium']] = df_norm
 
-#нелинейные преобразования
-#quantile_transformer = preprocessing.QuantileTransformer(n_quantiles = 100, random_state=0).fit(data)
-quantile_transformer = preprocessing.QuantileTransformer(n_quantiles = 100, random_state=0, output_distribution='normal').fit(data)
-data_quantile_scaled = quantile_transformer.transform(data)
+#quantile transformer
+qt = preprocessing.QuantileTransformer(n_quantiles=100, random_state=0, output_distribution='normal').fit(data)
+data_qt = qt.transform(data)
 fig6, axs = plt.subplots(2,3)
-axs[0, 0].hist(data_quantile_scaled[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(data_quantile_scaled[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(data_quantile_scaled[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(data_quantile_scaled[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(data_quantile_scaled[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(data_quantile_scaled[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
+for i, ax in enumerate(axs.flatten()):
+    ax.hist(data_qt[:,i], bins = n_bins)
+    ax.set_title(data.columns[i])
 # plt.show()
 
-
-#нелинейные преобразования с помощью PowerTransformer
-power_transformer = preprocessing.PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit(data)
-power_transformer_scaled = power_transformer.transform(data)
+#PowerTransformer
+pt = preprocessing.PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit(data)
+data_pt = pt.transform(data)
 fig7, axs = plt.subplots(2,3)
-axs[0, 0].hist(power_transformer_scaled[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(power_transformer_scaled[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(power_transformer_scaled[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(power_transformer_scaled[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(power_transformer_scaled[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(power_transformer_scaled[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
+for i, ax in enumerate(axs.flatten()):
+    ax.hist(data_pt[:,i], bins = n_bins)
+    ax.set_title(data.columns[i])
 # plt.show()
 
-#дискретизация признаков
-k_bins_discretizer = preprocessing.KBinsDiscretizer(n_bins=[3,4,3,10,2,4], encode='ordinal').fit(data)
-k_bins_discretizer_scaled = k_bins_discretizer.transform(data)
+#discretizer
+k_bins = preprocessing.KBinsDiscretizer(n_bins=[3,4,3,10,2,4], encode='ordinal').fit(data)
+data_k_bins = k_bins.transform(data)
 fig8, axs = plt.subplots(2,3)
-axs[0, 0].hist(k_bins_discretizer_scaled[:,0], bins = n_bins)
-axs[0, 0].set_title('age')
-axs[0, 1].hist(k_bins_discretizer_scaled[:,1], bins = n_bins)
-axs[0, 1].set_title('creatinine_phosphokinase')
-axs[0, 2].hist(k_bins_discretizer_scaled[:,2], bins = n_bins)
-axs[0, 2].set_title('ejection_fraction')
-axs[1, 0].hist(k_bins_discretizer_scaled[:,3], bins = n_bins)
-axs[1, 0].set_title('platelets')
-axs[1, 1].hist(k_bins_discretizer_scaled[:,4], bins = n_bins)
-axs[1, 1].set_title('serum_creatinine')
-axs[1, 2].hist(k_bins_discretizer_scaled[:,5], bins = n_bins)
-axs[1, 2].set_title('serum_sodium')
+for i, ax in enumerate(axs.flatten()):
+    ax.hist(data_k_bins[:,i], bins = n_bins)
+    ax.set_title(data.columns[i])
 plt.show()
+
+
 
